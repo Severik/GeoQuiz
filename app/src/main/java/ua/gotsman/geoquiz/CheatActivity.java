aa7e08b02990bb70fc291b4f8cab51548422c007
Денис <gotsman.denis@gmail.com>
2016-12-28 13:20:41 +0200

    beta

--- app/src/main/java/ua/gotsman/geoquiz/CheatActivity.java
+++ app/src/main/java/ua/gotsman/geoquiz/CheatActivity.java
@@ -1,13 +1,22 @@
 package ua.gotsman.geoquiz;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class CheatActivity extends AppCompatActivity {
 
+    public static final String EXTRA_ANSWER_IS_TRUE = "ua.gotsman.geoquiz.answer_is_true";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
+    }
+
+    public static Intent newIntent(Context packageContext, boolean answerIsTrue) {
+        Intent i = new Intent(packageContext, CheatActivity.class);
+        return i.putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue);
     }
 }
